03f75bebfde593eaf800ce9a189a40236ef442e5
Newton Rono <45383068+NewtonRono@users.noreply.github.com>
2019-10-29 16:46:18 +0300

    Update MainActivity.java

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -84,13 +84,13 @@ public class MainActivity extends AppCompatActivity {
     // TODO - Task 1 - Show Store Information Activity
     //
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         int id = item.getItemId();
-
-        if (id == R.id.info) {
-            Intent infoIntent = new Intent(MainActivity.this, InfoActivity.class);
+        if (id == R.id.info){
+            Intent infoIntent = new Intent(this,InfoActivity.class);
             startActivity(infoIntent);
         }
 
